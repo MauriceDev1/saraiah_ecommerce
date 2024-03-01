04c9286adb4d1d871a3b8f600941232f25f38941
MauriceDev1 <pschreuder11@gmail.com>
2024-03-01 15:09:40 +0200

    added shops page and added links to navigation

--- src/components/navigation/Nav.tsx
+++ src/components/navigation/Nav.tsx
@@ -1,10 +1,31 @@
-import {Component} from "solidjs"
+import {Component, For } from "solid-js"
 
 const Nav:Component = () => {
+	const Links = [
+		{
+			id:1,
+			title: 'Home',
+			link: '/',
+		},
+		{
+			id:2,
+			title: 'Shops',
+			link: '/shops',
+		}
+	];
+
 	return (
 		<nav class="w-full py-2">
 			<div class="w-10/12 m-auto">
 				Navigation
+
+				<ul>
+					<For each={Links}>{(l) => (
+						<a href={l.link}>
+							<li>{l.title}</li>
+						</a>
+					)}</For>
+				</ul>
 			</div>
 		</nav>
 	)
