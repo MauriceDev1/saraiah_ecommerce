c3190d9d5a977f8d1fb1b382b694f7e01e5f4afc
MauriceDev1 <pschreuder11@gmail.com>
2024-03-04 12:21:56 +0200

    added location logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Component } from 'solid-js'
 import Hero from '../components/general/Hero'
+import LocationComponent from '../components/LocationComponent';
 
 const Home:Component = () => {
 	return (
 		<>
 			<Hero />
+			<LocationComponent />
 			Home
 		</>
 	)
