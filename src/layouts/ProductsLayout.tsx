dc97b6d2d27f475360c45205ba79e9a143628dae
kristianrobyn <kristyslittlethings@gmail.com>
2024-03-22 07:28:29 +0200

    fixed product laoding

--- src/layouts/ProductsLayout.tsx
+++ src/layouts/ProductsLayout.tsx
@@ -1,8 +1,9 @@
 import { Component, For, createEffect, createSignal } from "solid-js"
-import ProductMenu from "../components/pageComponents/producst/ProductMenu"
+// import ProductMenu from "../components/pageComponents/producst/ProductMenu"
 import { useParams } from "@solidjs/router"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "../firebase/config"
+import LoadingScreen from "../components/general/LoadingScreen"
 
 const ProductsLayout: Component = () => {
     const [productsList,setProductsList] = createSignal<any[]>([]);
@@ -28,36 +29,41 @@ const ProductsLayout: Component = () => {
     return (
         <div class="w-11/12 m-auto pt-16 lg:pt-32 flex gap-5 py-10">
             {/* <ProductMenu /> */}
-            <div class="w-full flex flex-wrap">
-            
-                {productsList().length > 0 
-                    ?
-                        <For each={productsList()}>{
-                            (d) => 
-                                <a href={`/product/${d.id}`} class="w-full lg:w-1/6">
-                                    <div class="w-full shadow bg-white">
-                                        <div class="w-full bg-gray-200">
-                                            <img src={d.images[0]} alt={d.title}/>
-                                        </div>
-                                        <div class="px-2 pb-2">
-                                            <h3 class="text-center text-md pt-2 pb-1">
-                                                {d.name}
-                                            </h3>
-                                            <p class="text-sm text-center">
-                                                {d.summary}
-                                            </p>
-                                        </div>
+            {loading() 
+                ?
+                    <div class="w-full flex flex-wrap">
+                    
+                        {productsList().length > 0 
+                            ?
+                                <For each={productsList()}>{
+                                    (d) => 
+                                        <a href={`/product/${d.id}`} class="w-full lg:w-1/6">
+                                            <div class="w-full shadow bg-white">
+                                                <div class="w-full bg-gray-200">
+                                                    <img src={d.images[0]} alt={d.title}/>
+                                                </div>
+                                                <div class="px-2 pb-2">
+                                                    <h3 class="text-center text-md pt-2 pb-1">
+                                                        {d.name}
+                                                    </h3>
+                                                    <p class="text-sm text-center">
+                                                        {d.summary}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                }</For>
+                            :
+                                <div class="w-full h-full flex">
+                                    <div class="m-auto">
+                                        This store currently has no products
                                     </div>
-                                </a>
-                        }</For>
-                    :
-                        <div class="w-full h-full flex">
-                            <div class="m-auto">
-                                This store currently has no products
-                            </div>
-                        </div>
-                }
-            </div>
+                                </div>
+                        }
+                    </div>
+                :
+                    <LoadingScreen />
+            }
         </div>
     )
 }
